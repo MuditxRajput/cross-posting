699ec441fdb964957bb056e0dbab1c2041d9264e
muditxrajput <muditrajputpersonal@gmail.com>
2025-02-13 01:43:07 +0530

    refactor: update API endpoint for queue creation in StepForm component

--- apps/web/app/components/stepForm.tsx
+++ apps/web/app/components/stepForm.tsx
@@ -130,7 +130,7 @@ const StepForm = ({ image, aspectRatio }: StepFormProps) => {
         const userEmail = session.data?.user?.email;
         const mediaType = fileType;
         console.log("image is uploaded", formData.image);
-        const resp = await fetch('arn:aws:apigateway:us-east-1::/apis/a8d7v7e145/routes/2fz4ssl/queue-creation', {
+        const resp = await fetch('https://a8d7v7e145.execute-api.us-east-1.amazonaws.com/queue-creation', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
